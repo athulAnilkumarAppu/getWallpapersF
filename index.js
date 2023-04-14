0d948876e83848fd8363f03b424298cf46fc64f2
athulAnilkumarAppu <athulAnilkumarAppu@gmail.com>
2023-04-14 22:40:29 +0530

    SAs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = process.env.PORT || 3000 || 3001
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended:true}))

eb55c7172a89e9b48dfc3caf77c38b51bacb109b
athulAnilkumarAppu <athulAnilkumarAppu@gmail.com>
2023-04-14 22:25:53 +0530

    initial

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 3000 || 3001
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+
+app.get('/getWallpapers', (req, res)=> {
+    res.sendFile(filepath)
+})
+
+
+app.listen(port, console.log(`listening to port ${port}`))
